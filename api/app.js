55ddf4c66aeee50ab087629c93d829cec3747802
linh.lm <linh.lm@gnt.com.vn>
2018-09-03 11:14:51 +0700

    [API] change Router

--- api/app.js
+++ api/app.js
@@ -21,10 +21,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
+
+/* Router */
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
-app.use('/get/:userID', usersRouter)
-app.use('/add', usersRouter)
+app.use('/user', usersRouter)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
